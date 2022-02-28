e845bfef003ad16395dee38ef2466793f314893b
Eliphaz <eliahlander3@gmail.com>
2022-02-28 14:34:37 -0700

    finished all steps

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const cors = require('cors')
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const SERVER_PORT = 4000
 
@@ -11,7 +12,13 @@ app.listen(SERVER_PORT, () =>{
     console.log(`server up on ${SERVER_PORT}`)
 })
 
-app.get("/api/users", (req,res)=>{
+app.get("/api/users", (req, res) => {
     let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
-    res.status(200).send(friends);
+    res.status(200).send(friends)
+    console.log(friends)
+  })
+
+app.get("/weather/:temperature", (req,res) =>{
+    const temp = `<h3>It was ${req.params.temperature} today</h3>`
+    res.status(200).send(temp)
 })
